28e494215ae5939e40187dbaa4f75f5807717a71
Zhuohui Yu <agassi21@gmail.com>
2012-07-04 16:33:32 +0800

    new target

--- foo/QGLViewController.m
+++ foo/QGLViewController.m
@@ -18,12 +18,26 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	NSString *showString;
+	
+#ifdef PRO
+	showString = @"This is pro version,you are a rich man!";
+#else
+	showString = @"This is lite version.";
+#endif
+	
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil message:showString delegate:nil cancelButtonTitle:@"cancel" otherButtonTitles:nil, nil];
+	
+	[alertView show];
+
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+	
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
